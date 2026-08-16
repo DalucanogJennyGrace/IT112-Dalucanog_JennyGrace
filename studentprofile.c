4ced9d2c28989b123040d1c4fe1a5a15e0fac1ae
DalucanogJennyGrace <z2y8ysrgbk@privaterelay.appleid.com>
2026-08-16 22:58:18 +0800

    Create studentprofile.c

---
+++ studentprofile.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    printf("================================\n");
+    printf("                STUDENT PROFILE\n");
+    printf("================================\n");
+    printf("Name: Jenny Grace J. Dalucanog\n");
+    printf("Course and Section: BSIT-1R13\n");
+    printf("Year Level: 1st Year\n");
+    printf("================================\n");
+    printf("             SOFTWARE DEVELOPER\n");
+    printf("================================\n");
+    return 0;
+}
